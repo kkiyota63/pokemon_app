6560419efece45fe16afc3eb59bd80a13545d057
Koki Kiyota <82688821+kkiyota63@users.noreply.github.com>
2023-09-22 14:31:01 +0900

    settingページ遷移実装

--- lib/main.dart
+++ lib/main.dart
@@ -72,33 +72,82 @@ class PokeList extends StatelessWidget {
   }
 }
 
-class Settings extends StatelessWidget {
+//settingsのページ設定
+class Settings extends StatefulWidget {
   const Settings({Key? key}) : super(key: key);
+  @override
+  _SettingsState createState() => _SettingsState();
+}
+
+class _SettingsState extends State<Settings> {
+  ThemeMode _themeMode = ThemeMode.system;
   @override
   Widget build(BuildContext context) {
     return ListView(
       children: [
-        const ListTile(
-          leading: Icon(Icons.lightbulb),
-          title: Text('Dark/Light Mode'),
-        ),
-        SwitchListTile(
-          title: const Text('Switch'),
-          value: true,
-          onChanged: (yes) => {},
-        ),
-        CheckboxListTile(
-          title: const Text('Checkbox'),
-          value: true,
-          onChanged: (yes) => {},
-        ),
-        RadioListTile(
-          title: const Text('Radio'),
-          value: true,
-          groupValue: true,
-          onChanged: (yes) => {},
+        ListTile(
+          leading: const Icon(Icons.lightbulb),
+          title: const Text('Dark/Light Mode'),
+          trailing: Text((_themeMode == ThemeMode.system)
+              ? 'System'
+              : (_themeMode == ThemeMode.dark ? 'Dark' : 'Light')),
+          onTap: () async {
+            var ret = await Navigator.of(context).push<ThemeMode>(
+              MaterialPageRoute(
+                builder: (context) => ThemeModeSelectionPage(mode: _themeMode),
+              ),
+            );
+            setState(() => _themeMode = ret!);
+          },
         ),
       ],
+    );
+  }
+}
+
+//System / Dark / Lightの選択ぺージ
+//RadioListTile の groupValue を保持する必要があるので、StatefulWidget にする
+class ThemeModeSelectionPage extends StatefulWidget {
+  const ThemeModeSelectionPage({
+    Key? key,
+    required this.mode,
+  }) : super(key: key);
+  final ThemeMode mode;
+
+  @override
+  _ThemeModeSelectionPageState createState() => _ThemeModeSelectionPageState();
+}
+
+class _ThemeModeSelectionPageState extends State<ThemeModeSelectionPage> {
+  late ThemeMode _current;
+  @override
+  void initState() {
+    super.initState();
+    _current = widget.mode;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            ListTile(
+              leading: IconButton(
+                icon: const Icon(Icons.arrow_back),
+                onPressed: () => Navigator.pop<ThemeMode>(context, _current),
+              ),
+            ),
+            RadioListTile<ThemeMode>(
+              value: ThemeMode.system,
+              groupValue: _current,
+              title: const Text('System'),
+              onChanged: (val) => {setState(() => _current = val!)},
+            ),
+            // Radioいくつかつづきます・・・・
+          ],
+        ),
+      ),
     );
   }
 }
